fd73bb0c91222763759250c4aeb7add036e1ef26
Michael Braun <braunm@smu.edu>
2015-04-30 17:12:02 -0500

    really making a lot of progress on the vignette

--- inst/examples/summarize_timings.R
+++ inst/examples/summarize_timings.R
@@ -20,24 +20,41 @@ tab2 <- gather(tab, stat, ms, c(setup:hess,hess_f:total_df)) %>%
   summarize(mean=mean(ms), sd=sd(ms), lower=mean-1.96*sd, upper=mean+1.96*sd)
 
 
-## D2 <- subset(data.frame(tab2), stat %in% c("nvars","setup","colors","f","df","hess",
-##                                "hess_df","total","total_f","total_df"))
+D2 <- subset(data.frame(tab2), stat %in% c("f", "df", "hess",
+                                           "colors", "setup","hess_df"))
+D2$stat <- revalue(D2$stat, c("f"="Function", "df"="Gradient", "hess"="Hessian",
+                              "colors"="Partitioning",
+                              "setup"="Initialization",
+                              "hess_df"="Hessian/Gradient"))
+D2$stat <- relevel(D2$stat, "Hessian/Gradient") %>%
+  relevel("Initialization") %>%
+  relevel("Partitioning") %>%
+  relevel("Hessian") %>%
+  relevel("Gradient") %>%
+  relevel("Function")
 
-D2 <- data.frame(tab2)
 
-P2 <- ggplot(D2, aes(x=N,y=mean, color=as.factor(k))) %>%
-  + geom_line() %>%
+
+P2 <- ggplot(D2, aes(x=N,y=mean, color=as.factor(k), linetype=as.factor(k))) %>%
+  + geom_line(size=.4) %>%
   + scale_x_continuous("Number of heterogeneous units") %>%
   + scale_y_continuous("Computation time (milliseconds)") %>%
-  + facet_wrap(~stat, scales="free")
+  + guides(color=guide_legend("k"), linetype=guide_legend("k")) %>%
+  + facet_wrap(~stat, scales="free") %>%
+  + theme(text=element_text(size=8))
 
 
-P3 <- ggplot(filter(D2,k==4), aes(x=N)) %>%
-  + geom_line(aes(y=mean), linetype=1) %>%
-  + geom_line(aes(y=upper), linetype=3) %>%
-  + geom_line(aes(y=lower), linetype=3) %>%
-  + facet_wrap(~stat, scales="free")
-
-
-
+pdf(file="vignettes/timings.pdf", width=6.5, height=4)
 print(P2)
+dev.off()
+
+
+## P3 <- ggplot(filter(D2,k==4), aes(x=N)) %>%
+##   + geom_line(aes(y=mean), linetype=1) %>%
+##   + geom_line(aes(y=upper), linetype=3) %>%
+##   + geom_line(aes(y=lower), linetype=3) %>%
+##   + facet_wrap(~stat, scales="free", nrow=2, ncol=3)
+
+
+
+
